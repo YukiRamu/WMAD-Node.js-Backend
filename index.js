1bd119ee644e04ded4bf77f2250fae1b7730abad
Yuki <yukipooh4120@gmail.com>
2021-08-24 02:21:00 -0700

    basic auth added. working only on backend

--- index.js
+++ index.js
@@ -2,23 +2,7 @@ const express = require("express");
 const basicAuth = require("express-basic-auth");
 const cors = require('cors');
 const app = express();
-const port = 3001;
-
-//****************** Use Router ***************** */
-const secureRoute = express.Router();
-secureRoute.use(basicAuth, (req, res) => {
-
-});
-
-const insecureRoute = express.Router();
-
-secureRoute.put("/secret", () => {
-
-});
-
-insecureRoute.put("/public", () => {
-
-});
+const port = 5000;
 
 //avoid cross domain error - when we edit frontend and backend at the same time
 app.use(cors());
@@ -26,57 +10,77 @@ app.use(express.json());
 
 //setting up basic auth - as soon as we put this, the website become password-protected
 app.use(basicAuth({
-  // authorizer: (username, password) => {
-  //   let _username = "Yuki";
-  //   let _password = "mypassword";
-  //   if ((basicAuth.safeCompare(_username, username) === true) && (basicAuth.safeCompare(_password, password) === true)) {
-  //     //both match
-  //     console.log("both password and username match");
-  //   }
-  // },
-  users: { "admin": "password", "admin2": "password2" },
-  challenge: true
+  users: { "admin": "password", "yuki": "password" },
+  //challenge: true,
+  unauthorizedResponse: () => {
+    return "Unauthorized!!";
+  },
+  authorizer: (username, password) => {
+    console.log(" #1. usernamd and password is", username, password);
+    const admin = {
+      _username: "yuki",
+      _password: "password"
+    };
+    const userMatches = basicAuth.safeCompare(username, admin._username);
+    const passwordMatches = basicAuth.safeCompare(password, admin._password);
+    if (userMatches && passwordMatches) {
+      //both match
+      console.log("#2. both password and username match", userMatches, passwordMatches);
+      return userMatches & passwordMatches;
+    }
+  },
 }));
 
-//Challenge
-// app.use(basicAuth({
-//   users: { "admin": "password", "admin2": "password2" },
-//   challenge: true
-// }));
-
-//setting up routes
-app.get("/", (req, res, next) => {
-  res.send("Authorized!!!");
-});
-
-// const getVersion = () => {
-//   return "1.0";
-// };
-// app.get("/version", getVersion); // can call function
-
-app.post("/login", (req, res, next) => {
-  console.log(req);
-  console.log(req.body);
-  // res.send("I got", req.body);
-  res.send(`I got: ${JSON.stringify(req.body)}`);
-});
-
-app.put("/login", (req, res, next) => {
-  console.log(req);
-  console.log(req.body);
-  // res.send("I got", req.body);
-  res.send(`I got: ${JSON.stringify(req.body)}`);
-});
-
-app.get("/version", (req, res) => {
-  res.send("1.0");
-});
-
-app.get("testName", (req, res) => {
-  res.send(users);
+//after authorized, send login data to the server
+app.put("/api/login", (req, res) => {
+  let param = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  console.log("#3. req.auth is ", param);
+  if (param.username === "yuki" && param.password === "password") {
+    res.send("yuki logged in!");
+  }
 });
 
 //server listens
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+//================ Templates ==================
+//****************** Use Router ***************** */
+// const secureRoute = express.Router();
+// secureRoute.use(basicAuth, (req, res) => {
+
+// });
+
+// const insecureRoute = express.Router();
+
+// secureRoute.put("/secret", () => {
+
+// });
+
+// insecureRoute.put("/public", () => {
+
+// });
+
+// const getVersion = () => {
+//   return "1.0";
+// };
+// app.get("/version", getVersion); // can call function
+
+// app.post("/login", (req, res, next) => {
+//   console.log(req);
+//   console.log(req.body);
+//   // res.send("I got", req.body);
+//   res.send(`I got: ${JSON.stringify(req.body)}`);
+// });
+
+// app.put("/login", (req, res, next) => {
+//   console.log(req);
+//   console.log(req.body);
+//   // res.send("I got", req.body);
+//   res.send(`I got: ${JSON.stringify(req.body)}`);
+// });
